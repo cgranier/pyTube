544b0c948ba29a73896950d5e5d42b7d97b7ed14
Carlos Granier <granier@gmail.com>
2019-11-26 15:04:53 -0500

    Dailymotion Playlist Get works

--- dm_playlists.py
+++ dm_playlists.py
@@ -9,6 +9,8 @@ import pandas as pd
 import dailymotion
 
 import playlists
+# PLAYLISTS = playlists.DAILYMOTION
+PLAYLISTS = ['x6f0b9']
 
 # First version - no functions, etc.
 
@@ -28,7 +30,37 @@ d.set_grant_type('password', api_key, api_secret,
 # this one reorders the playlist [https://stackoverflow.com/questions/59040674/how-to-reorder-a-playlist]
 # d.post('/playlist/{PLAYLIST_ID}/videos', {'ids' : '{VIDEO_ID},...,{VIDEO_ID}'})
 
-playlist_id = 'x6f0b9'
-videos = d.get('/playlist/x6f0b9/videos', {'fields': 'id,title,url'})
+# playlist_id = 'x6f0b9'
+# videos = d.get('/playlist/x6f0b9/videos', {'fields': 'id,title,url'})
+# print(json.dumps(videos))
 
-print(json.dumps(videos))
+dm_get_playlist_API = '/playlist/{0}/videos'
+# dm_get_playlist_fields = {'fields': 'id,title,url', 'page'}
+
+def get_playlist_videos(PLAYLIST_ID):
+    playlist_videos = []
+    page = 1
+    limit = 100
+    has_more = True
+
+    while has_more:
+        response = d.get(dm_get_playlist_API.format(PLAYLIST_ID), { 'fields': 'id,title,url', 'page': page, 'limit': limit})
+        for video in response['list']:
+            playlist_videos.append(video)
+        page += 1
+        has_more = response['has_more']
+
+    return playlist_videos
+
+
+def main():
+    for playlist in PLAYLISTS:
+        playlist_videos = get_playlist_videos(playlist)
+        for video in playlist_videos:
+            video_id = video.get('id')
+            video_title = video.get('title')
+            video_url = video.get('url')
+            print(f'Video id: {video_id} * Video title: {video_title} * Video url: {video_url}')
+
+if __name__ == "__main__":
+    main()
