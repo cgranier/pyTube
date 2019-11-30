6d71bf4608f6efc8894275ccac44ef3887b4bdc4
Carlos Granier <granier@gmail.com>
2019-11-30 11:07:45 -0500

    Cleaned up functions, add print to CSV, included video descriptions.

--- dm_playlists.py
+++ dm_playlists.py
@@ -9,10 +9,8 @@ import pandas as pd
 import dailymotion
 
 import playlists
-# PLAYLISTS = playlists.DAILYMOTION
-PLAYLISTS = ['x6f0cn']
-
-# First version - no functions, etc.
+PLAYLISTS = playlists.DAILYMOTION
+# PLAYLISTS = ['_']
 
 # SETUP
 
@@ -26,17 +24,8 @@ api_url = config.BASE_URL
 
 d.set_grant_type('password', api_key, api_secret,
     scope=['userinfo'], info={'username': api_username, 'password': api_password})
-    
-# Get a list of videos in a Playlist
-# this one reorders the playlist [https://stackoverflow.com/questions/59040674/how-to-reorder-a-playlist]
-# d.post('/playlist/{PLAYLIST_ID}/videos', {'ids' : '{VIDEO_ID},...,{VIDEO_ID}'})
-
-# playlist_id = 'x6f0b9'
-# videos = d.get('/playlist/x6f0b9/videos', {'fields': 'id,title,url'})
-# print(json.dumps(videos))
 
 dm_playlist_API = '/playlist/{0}/videos'
-# dm_get_playlist_fields = {'fields': 'id,title,url', 'page'}
 
 def get_playlist_videos(PLAYLIST_ID):
     playlist_videos = []
@@ -45,7 +34,7 @@ def get_playlist_videos(PLAYLIST_ID):
     has_more = True
 
     while has_more:
-        response = d.get(dm_playlist_API.format(PLAYLIST_ID), { 'fields': 'id,title,url', 'page': page, 'limit': limit})
+        response = d.get(dm_playlist_API.format(PLAYLIST_ID), {'fields': 'id,title,url,description', 'page': page, 'limit': limit})
         for video in response['list']:
             playlist_videos.append(video)
         page += 1
@@ -60,6 +49,7 @@ def get_video_list(playlist_videos):
         video_id = video.get('id')
         video_title = video.get('title')
         video_url = video.get('url')
+        video_description = video.get('description')
         if 'Episodio' in video_title:
             video_episode = int(re.search('(Episodio\s)(\d+)(\s\|)',video_title).group(2))
         elif 'Episode' in video_title:
@@ -67,47 +57,33 @@ def get_video_list(playlist_videos):
         else:
             sys.exit('Non-standard title format - reorder by hand.')
         
-        data_row = {'video_id':video_id,'video_title':video_title,'video_url':video_url,'video_episode':video_episode}
+        data_row = {'video_id':video_id,'video_title':video_title,'video_url':video_url,'video_episode':video_episode,'video_description':video_description}
         video_list.append(data_row)
 
     return video_list
+
+def reorder_videos(playlist_id, video_ids):
+    # Thanks to Reddit user https://www.reddit.com/user/semicolonator/
+    # for his help with this code to create the API call
+    videos_in_order = {'ids': ",".join(["%s" % i for i in video_ids])}
+    order_api = d.post(dm_playlist_API.format(playlist_id), videos_in_order)
+    print(f'Playlist {playlist_id} ordered!')
+    return
+
+def create_ordered_csv(playlist_id, video_df):
+    csv_filename = 'dm_playlist_{0}.csv'.format(playlist_id)
+    video_df.to_csv(csv_filename, encoding='utf-8', header = True, index = None)
+    print(f'Ordered playlist {playlist_id} saved to csv file {csv_filename}!')
+    return
 
 def main():
     for playlist in PLAYLISTS:
         playlist_videos = get_playlist_videos(playlist)
         video_list = get_video_list(playlist_videos)
         video_df = pd.DataFrame(video_list).sort_values(by=['video_episode'])
-
-        print(video_df['video_id'])
-
-        # CALL API POST to reorder videos here
-
-        # for row in video_df.itertuples(name='video_to_update'):
-        #     print(f'Video id: {video_id} * Video title: {video_title} * Video url: {video_url}')
-
-        # Reddit idea to pass video_ids to APi:
-        # https://www.reddit.com/user/semicolonator/
-        # v = df["video_id"].tolist()
-        # arg1 = '/playlist/{PLAYLIST_ID}/videos'
-        # arg2 = {'ids': ",".join(["{%s}" % i for i in v])}
-        # d.post(arg1, arg2)
-
         video_ids = video_df['video_id'].tolist()
-        print(video_ids)
-
-        # dm_playlist_API = '/playlist/{0}/videos'
-        # d.post('/playlist/{PLAYLIST_ID}/videos', {'ids' : '{VIDEO_ID},...,{VIDEO_ID}'})
-
-        videos_in_order = {'ids': ",".join(["%s" % i for i in video_ids])}
-
-        print(videos_in_order)
-        order_api = d.post(dm_playlist_API.format(playlist), videos_in_order)
-
-        # for video in playlist_videos:
-        #     video_id = video.get('id')
-        #     video_title = video.get('title')
-        #     video_url = video.get('url')
-        #     print(f'Video id: {video_id} * Video title: {video_title} * Video url: {video_url}')
+        reorder_videos(playlist, video_ids)
+        create_ordered_csv(playlist, video_df)
 
 if __name__ == "__main__":
     main()
